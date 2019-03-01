ab6a86aba734f583681c3362cc650e35c917bfdb
Andrii Vasyliev <sol@hiqdev.com>
2019-03-01 17:54:45 +0200

    Moved config to root dir

--- config/tests.php
+++ config/tests.php
@@ -2,15 +2,15 @@
 
 return [
     'id' => 'testapp',
-    'basePath' => dirname(__DIR__, 2),
-    'vendorPath' => dirname(__DIR__, 2) . "/vendor",
+    'basePath' => dirname(__DIR__),
+    'vendorPath' => dirname(__DIR__) . "/vendor",
     'aliases' => [
         '@bower' => '@vendor/bower-asset'
     ],
     'components' => [
         'request' => [
             'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
-            'scriptFile' => dirname(__DIR__, 2) .'/tests/index.php',
+            'scriptFile' => dirname(__DIR__) .'/tests/index.php',
             'scriptUrl' => '/index.php',
         ],
         'assetManager' => [
